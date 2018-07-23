f165685daf5a8fc42b125b8c14f83c22c486350e
Karim Ben Ammar <karim.benammar@esprit.tn>
2018-07-23 18:54:41 +0100

    Sign up & Login UI minor changes

--- app/src/main/java/com/monresto/acidlabs/monresto/UI/User/FragmentRegisterLoginInfo.java
+++ app/src/main/java/com/monresto/acidlabs/monresto/UI/User/FragmentRegisterLoginInfo.java
@@ -3,7 +3,9 @@ package com.monresto.acidlabs.monresto.UI.User;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,7 +23,17 @@ public class FragmentRegisterLoginInfo extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup root;
         root = (ViewGroup) inflater.inflate(R.layout.fragment_register_login_info, container, false);
-
         return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        System.out.println(((ViewPager)view.getParent()).getLayoutParams().height);
+        System.out.println(view.getLayoutParams().height);
+
+        //view.setLayoutParams(new ViewPager.LayoutParams(ViewPager.LayoutParams.MATCH_PARENT, ViewPager.LayoutParams.WRAP_CONTENT));
     }
 }
